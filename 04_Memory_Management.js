7e5362b196cde6c3fa50bc510a02a4f90211fa74
AyushRawat1718 <ayushrawat1718@gmail.com>
2024-05-25 21:29:24 +0530

    Minor Changes

--- 04_Memory_Management.js
+++ 04_Memory_Management.js
@@ -3,5 +3,5 @@ We can categories data types into two parts, on the basis of Memory Allocation i
 
 Primitive Data => These data types are stored in stack memory, and whenever they are copied or passed in a function, they are passed by value i.e. a copy of these are created and then passed on. So we can say that any changes made inside in the function or in the copied value will not effect the original value.
 
-Non-Primitive => These data types are stored in heap memory and their reference(address of the data in heap memory) is stored in stack memory. So whenever they are copied or passed in a function, then there reference is passed. So we can say that any changes made inside the function or in the copied value will effect the original value. 
+Non-Primitive => These data types are stored in heap memory and their reference(address of the data in heap memory) is stored in stack memory. So whenever they are copied or passed in a function, then there reference is passed. That's why any changes made inside the function or in the copied value will effect the original value. 
 */
